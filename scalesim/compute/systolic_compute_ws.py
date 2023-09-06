53a7d2522b90f0bc32be7a9a1f63fab0a31bf1ab
ritikraj7 <ritwikraaj@gmail.com>
2023-09-06 16:58:06 -0400

    Fixing weight stationary and modifying CI test

--- scalesim/compute/systolic_compute_ws.py
+++ scalesim/compute/systolic_compute_ws.py
@@ -181,7 +181,7 @@ class systolic_compute_ws:
         inter_fold_gap_prefix = self.arr_row
         inter_fold_gap_prefix_mat = np.ones((inter_fold_gap_prefix, self.arr_row)) * -1
 
-        inter_fold_gap_suffix = self.arr_row + self.arr_col - 2
+        inter_fold_gap_suffix = self.arr_col - 1
         #The last input needs self.arr_row - 1 cycles to reach the last column of PE array and then self.arr_col - 1 cycles to reduce along the last column.
 
         inter_fold_gap_suffix_mat = np.ones((inter_fold_gap_suffix, self.arr_row)) * -1
---
+++ test/scripts/function_test.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+path="./"
+
+sed -i "s/run_name = scale_example_run_32x32_os/run_name = scale_example_run_32x32_ws/" $path/configs/scale.cfg
+sed -i "s/Dataflow : os/Dataflow : ws/" $path/configs/scale.cfg
+sed -i "s/save_disk_space=True/save_disk_space=False/" $path/scalesim/scale.py
+sed -i "s/verbose=True/verbose=False/" $path/scalesim/scale.py
+
+source $path/venv/bin/activate
+
+python3 $path/scalesim/scale.py -c $path/configs/scale.cfg -t $path/topologies/conv_nets/alexnet_part.csv 
+
+CMP1=$(cmp $path/../test_runs/scale_example_run_32x32_ws/BANDWIDTH_REPORT.csv $path/test/golden_trace/BANDWIDTH_REPORT.csv)
+CMP2=$(cmp $path/../test_runs/scale_example_run_32x32_ws/COMPUTE_REPORT.csv $path/test/golden_trace/COMPUTE_REPORT.csv)
+CMP3=$(cmp $path/../test_runs/scale_example_run_32x32_ws/DETAILED_ACCESS_REPORT.csv $path/test/golden_trace/DETAILED_ACCESS_REPORT.csv)
+CMP4=$(cmp $path/../test_runs/scale_example_run_32x32_ws/layer0/FILTER_DRAM_TRACE.csv $path/test/golden_trace/layer0/FILTER_DRAM_TRACE.csv)
+CMP5=$(cmp $path/../test_runs/scale_example_run_32x32_ws/layer0/FILTER_SRAM_TRACE.csv $path/test/golden_trace/layer0/FILTER_SRAM_TRACE.csv)
+CMP6=$(cmp $path/../test_runs/scale_example_run_32x32_ws/layer0/IFMAP_DRAM_TRACE.csv $path/test/golden_trace/layer0/IFMAP_DRAM_TRACE.csv)
+CMP7=$(cmp $path/../test_runs/scale_example_run_32x32_ws/layer0/IFMAP_SRAM_TRACE.csv $path/test/golden_trace/layer0/IFMAP_SRAM_TRACE.csv)
+CMP8=$(cmp $path/../test_runs/scale_example_run_32x32_ws/layer0/OFMAP_DRAM_TRACE.csv $path/test/golden_trace/layer0/OFMAP_DRAM_TRACE.csv)
+CMP9=$(cmp $path/../test_runs/scale_example_run_32x32_ws/layer0/OFMAP_SRAM_TRACE.csv $path/test/golden_trace/layer0/OFMAP_SRAM_TRACE.csv)
+
+
+if [ "$CMP1" != "" ]; then
+    echo "Output does not match!" > logfile.log
+    exit 125
+elif [ "$CMP2" != "" ]; then
+    echo "Output does not match!" > logfile.log
+    exit 125
+elif [ "$CMP3" != "" ]; then
+    echo "Output does not match!" > logfile.log
+    exit 125
+elif [ "$CMP4" != "" ]; then
+    echo "Output does not match!" > logfile.log
+    exit 125
+elif [ "$CMP5" != "" ]; then
+    echo "Output does not match!" > logfile.log
+    exit 125
+elif [ "$CMP6" != "" ]; then
+    echo "Output does not match!" > logfile.log
+    exit 125
+elif [ "$CMP7" != "" ]; then
+    echo "Output does not match!" > logfile.log
+    exit 125
+elif [ "$CMP8" != "" ]; then
+    echo "Output does not match!" > logfile.log
+    exit 125
+elif [ "$CMP9" != "" ]; then
+    echo "Output does not match!" > logfile.log
+    exit 125
+fi
