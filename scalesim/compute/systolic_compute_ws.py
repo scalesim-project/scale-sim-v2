98eb0c545f03b68c88806861b9ddd32d02d3c8f8
ritikraj7 <ritwikraaj@gmail.com>
2023-09-07 13:35:13 -0400

    Fixing weight stationary and modifying CI test

--- scalesim/compute/systolic_compute_ws.py
+++ scalesim/compute/systolic_compute_ws.py
@@ -182,8 +182,6 @@ class systolic_compute_ws:
         inter_fold_gap_prefix_mat = np.ones((inter_fold_gap_prefix, self.arr_row)) * -1
 
         inter_fold_gap_suffix = self.arr_col - 1
-        #The last input needs self.arr_row - 1 cycles to reach the last column of PE array and then self.arr_col - 1 cycles to reduce along the last column.
-
         inter_fold_gap_suffix_mat = np.ones((inter_fold_gap_suffix, self.arr_row)) * -1
 
         for fc in range(self.col_fold):
