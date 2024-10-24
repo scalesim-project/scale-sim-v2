1f1e20ea7f2558eedec9fb67a1ea3062bdf5afb4
NikhilChandraNcbs <48174779+NikhilChandraNcbs@users.noreply.github.com>
2024-10-24 16:16:57 -0400

    Structured sparsity CI/CD functionality and config files modified
    * Adding test skeleton for sparsity
    * CICD testing, report generation fixed
    * Modified config files to include sparsity support, added sparse_report check in .sh
    
    The initial 2 commits are present since I did not fetch the latest main branch locally

--- test/sparsity/scripts/function_test.sh
+++ test/sparsity/scripts/function_test.sh
@@ -11,12 +11,14 @@ python3 $path/scalesim/scale.py -c $path/configs/sparsity.cfg -t $path/topologie
 DIFF1=$(diff $path/sparsity_outputs/scalesim_sparsity/BANDWIDTH_REPORT.csv $path/test/sparsity/golden_trace/BANDWIDTH_REPORT.csv)
 DIFF2=$(diff $path/sparsity_outputs/scalesim_sparsity/COMPUTE_REPORT.csv $path/test/sparsity/golden_trace/COMPUTE_REPORT.csv)
 DIFF3=$(diff $path/sparsity_outputs/scalesim_sparsity/DETAILED_ACCESS_REPORT.csv $path/test/sparsity/golden_trace/DETAILED_ACCESS_REPORT.csv)
-DIFF4=$(diff $path/sparsity_outputs/scalesim_sparsity/layer0/FILTER_DRAM_TRACE.csv $path/test/sparsity/golden_trace/layer0/FILTER_DRAM_TRACE.csv)
-DIFF5=$(diff $path/sparsity_outputs/scalesim_sparsity/layer0/FILTER_SRAM_TRACE.csv $path/test/sparsity/golden_trace/layer0/FILTER_SRAM_TRACE.csv)
-DIFF6=$(diff $path/sparsity_outputs/scalesim_sparsity/layer0/IFMAP_DRAM_TRACE.csv $path/test/sparsity/golden_trace/layer0/IFMAP_DRAM_TRACE.csv)
-DIFF7=$(diff $path/sparsity_outputs/scalesim_sparsity/layer0/IFMAP_SRAM_TRACE.csv $path/test/sparsity/golden_trace/layer0/IFMAP_SRAM_TRACE.csv)
-DIFF8=$(diff $path/sparsity_outputs/scalesim_sparsity/layer0/OFMAP_DRAM_TRACE.csv $path/test/sparsity/golden_trace/layer0/OFMAP_DRAM_TRACE.csv)
-DIFF9=$(diff $path/sparsity_outputs/scalesim_sparsity/layer0/OFMAP_SRAM_TRACE.csv $path/test/sparsity/golden_trace/layer0/OFMAP_SRAM_TRACE.csv)
+DIFF4=$(diff $path/sparsity_outputs/scalesim_sparsity/SPARSE_REPORT.csv $path/test/sparsity/golden_trace/SPARSE_REPORT.csv)
+DIFF5=$(diff $path/sparsity_outputs/scalesim_sparsity/layer0/FILTER_DRAM_TRACE.csv $path/test/sparsity/golden_trace/layer0/FILTER_DRAM_TRACE.csv)
+DIFF6=$(diff $path/sparsity_outputs/scalesim_sparsity/layer0/FILTER_SRAM_TRACE.csv $path/test/sparsity/golden_trace/layer0/FILTER_SRAM_TRACE.csv)
+DIFF7=$(diff $path/sparsity_outputs/scalesim_sparsity/layer0/IFMAP_DRAM_TRACE.csv $path/test/sparsity/golden_trace/layer0/IFMAP_DRAM_TRACE.csv)
+DIFF8=$(diff $path/sparsity_outputs/scalesim_sparsity/layer0/IFMAP_SRAM_TRACE.csv $path/test/sparsity/golden_trace/layer0/IFMAP_SRAM_TRACE.csv)
+DIFF9=$(diff $path/sparsity_outputs/scalesim_sparsity/layer0/OFMAP_DRAM_TRACE.csv $path/test/sparsity/golden_trace/layer0/OFMAP_DRAM_TRACE.csv)
+DIFF10=$(diff $path/sparsity_outputs/scalesim_sparsity/layer0/OFMAP_SRAM_TRACE.csv $path/test/sparsity/golden_trace/layer0/OFMAP_SRAM_TRACE.csv)
+
 
 
 if [ "$DIFF1" != "" ]; then
@@ -54,5 +56,9 @@ elif [ "$DIFF8" != "" ]; then
 elif [ "$DIFF9" != "" ]; then
     echo "Output does not match!" 
     echo "$DIFF9" 
+    exit 1
+elif [ "$DIFF10" != "" ]; then
+    echo "Output does not match!" 
+    echo "$DIFF10" 
     exit 1
 fi
