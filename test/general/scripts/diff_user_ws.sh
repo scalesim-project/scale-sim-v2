fdb4bee27abade55addc778efb755bfb82caa38c
ritikraj7 <ritwikraaj@gmail.com>
2025-02-17 23:08:08 -0500

    solving syntax error in diff_user_ws.sh script

--- test/general/scripts/diff_user_ws.sh
+++ test/general/scripts/diff_user_ws.sh
@@ -2,9 +2,9 @@
 
 path="./"
 
-sed -i "s2/.*/run_name = scale_example_run_32x32_ws/" $path/configs/scale.cfg
-sed -i "s14/.*/Dataflow : ws/" $path/configs/scale.cfg
-sed -i "s25/.*/InterfaceBandwidth: USER/" $path/configs/scale.cfg
+sed -i "2s/.*/run_name = scale_example_run_32x32_ws/" $path/configs/scale.cfg
+sed -i "14s/.*/Dataflow : ws/" $path/configs/scale.cfg
+sed -i "25s/.*/InterfaceBandwidth: USER/" $path/configs/scale.cfg
 sed -i "s/save_disk_space=True/save_disk_space=False/" $path/scalesim/scale.py
 
 source venv/bin/activate
